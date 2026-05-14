4f2ed9af915303f1749a059b16c9167588c4df83
MarcosCasiraghi <mcasiraghi@itba.edu.ar>
2026-05-14 15:47:58 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# mendel_challenge
+
---
+++ src/main/java/org/example/mendel_challenge/MendelChallengeApplication.java
@@ -0,0 +1,13 @@
+package org.example.mendel_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MendelChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MendelChallengeApplication.class, args);
+	}
+
+}
